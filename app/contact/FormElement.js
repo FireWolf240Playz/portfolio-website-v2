aba3f62d37b89a437de42f51684b7417c3234858
FireWolf240Playz <a.yordanow67@gmail.com>
2024-05-30 20:29:22 +0300

    Implemented form submission handling

---
+++ app/contact/FormElement.js
@@ -0,0 +1,60 @@
+"use client";
+
+import { ValidationError } from "@formspree/react";
+
+function FormElement({ handleSubmit, state }) {
+  return (
+    <form
+      className="flex flex-col gap-5 justify-center h-screen"
+      onSubmit={handleSubmit}
+    >
+      <div className="flex flex-col justify-center gap-4 w-8/12">
+        <label htmlFor="name">What is your name?</label>
+        <input
+          id="name"
+          type="text"
+          name="name"
+          className="text-accent-600 border p-2 rounded-md"
+          placeholder="e.g John Doe"
+          required
+        />
+        <ValidationError prefix="Name" field="name" errors={state.errors} />
+      </div>
+      <div className="flex flex-col justify-center gap-4 w-8/12">
+        <label htmlFor="email">Your Email</label>
+        <input
+          id="email"
+          type="email"
+          name="email"
+          className="text-accent-600 border p-2 rounded-md"
+          placeholder="e.g me@example.com"
+          required
+        />
+        <ValidationError prefix="Email" field="email" errors={state.errors} />
+      </div>
+      <div className="flex flex-col justify-center gap-4 w-8/12">
+        <label htmlFor="inquiry">Please write your message here 👇</label>
+        <textarea
+          id="inquiry"
+          name="message"
+          className="rounded-md text-accent-600 p-2"
+          placeholder="Your message"
+          required
+        ></textarea>
+        <ValidationError
+          prefix="Message"
+          field="message"
+          errors={state.errors}
+        />
+      </div>
+      <button
+        type="submit"
+        className="w-8/12 flex justify-center rounded-md py-3 px-5 bg-accent-500  min-width: fit-content hover:bg-accent-600 transition-all duration-500 text-primary-100"
+      >
+        Submit
+      </button>
+    </form>
+  );
+}
+
+export default FormElement;
--- app/contact/page.js
+++ app/contact/page.js
@@ -6,14 +6,13 @@ import Form from "./Form";
 function Page() {
   return (
     <Section>
-      <>
-        <div className="flex flex-col gap-5 justify-center items-center h-screen">
-          <h1 className="inline-block text-4xl uppercase">
-            If you like what you see...{" "}
-          </h1>
-          <p className="text-2xl">Let&apos;s get in touch 👉</p>
-        </div>
-      </>
+      <div className="flex flex-col gap-5 justify-center items-center h-screen">
+        <h1 className="inline-block text-4xl uppercase">
+          If you like what you see...{" "}
+        </h1>
+        <p className="text-2xl">Let&apos;s get in touch 👉</p>
+      </div>
+
       <div className="w-full ">
         <Form />
       </div>
