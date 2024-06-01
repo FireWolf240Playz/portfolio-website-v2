f3d375827b2e26a18314967a933c0602cd9db62e
Alexander Yordanov <a.yordanow67@gmail.com>
2024-06-01 19:16:07 +0300

    Fixed some bugs

--- app/about/page.js
+++ app/about/page.js
@@ -1,12 +1,11 @@
 import Button from "../_components/Button";
-
 import AboutText from "../_components/AboutText";
 
 function Page() {
   return (
     <div className="flex bg-primary-800 px-8  items-center justify-around min-h-screen">
-      <div className="mt-[5rem] max-600:flex max-600:flex-col">
-        <p className="text-primary-100  mb-10 text-center font-bold text-2xl">
+      <div className="max-600:flex max-600:flex-col flex flex-col ">
+        <p className="text-primary-100 mb-10 text-center font-bold text-2xl">
           Front-end Developer based in Varna,Bulgaria! 😃
         </p>
         <AboutText />
