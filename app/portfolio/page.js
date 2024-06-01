7cdcffe93e2e4b308804a970be3c1ef0f7edc7f6
Alexander Yordanov <a.yordanow67@gmail.com>
2024-06-01 20:03:56 +0300

    Updated again portfolio links

--- app/portfolio/page.js
+++ app/portfolio/page.js
@@ -21,7 +21,7 @@ function Page() {
               Streamlines the reservation process, ensuring seamless 
               customer experiences and optimized workflow for the staff.`}
           githubLink="https://github.com/FireWolf240Playz/the-wild-oasis"
-          demoLink="https://the-wid-oasis-by-alexander-yordanov-3.netlify.app/login/"
+          demoLink="https://the-wid-oasis-by-alexander-yordanov-3.netlify.app/"
         />
 
         <ProjectImage image={image1} />
