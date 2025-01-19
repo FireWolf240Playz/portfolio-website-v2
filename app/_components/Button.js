f0bbccebcb6f6f09f55e2c04c797e0e188f4117d
Alexander Yordanov <a.yordanow67@gmail.com>
2025-01-19 13:43:25 +0200

    Added separation of the rendering

--- app/_components/Button.js
+++ app/_components/Button.js
@@ -7,17 +7,24 @@ function Button({
   className = "py-3 px-5 bg-accent-500 inline-block min-width: fit-content hover:bg-accent-600 transition-all duration-500 text-primary-100 rounded-md",
   extension = "",
   newTab = false,
+  onClick,
 }) {
-  return (
-    <button>
+  if (to) {
+    return (
       <Link
+        href={to}
         target={newTab ? "_blank" : "_self"}
         rel={newTab ? "noopener noreferrer" : ""}
         className={classNames(className, extension)}
-        href={to}
       >
         {children}
       </Link>
+    );
+  }
+
+  return (
+    <button onClick={onClick} className={classNames(className, extension)}>
+      {children}
     </button>
   );
 }
--- app/portfolio/page.js
+++ app/portfolio/page.js
@@ -1,17 +1,49 @@
+"use client";
+
+import { useState } from "react";
 import FrontendProjects from "@/app/portfolio/_sectionComponents/FrontendProjects";
 import FullStackProjects from "@/app/portfolio/_sectionComponents/FullStackProjects";
+import Button from "@/app/_components/Button";
 
 function Page() {
+  const [view, setView] = useState("all");
+
   return (
     <>
       <h1 className="text-4xl text-center mt-10 mb-10">
         Welcome to my portfolio page!
       </h1>
-      <div>
-        <h1 className="text-3xl text-center mt-10 mb-10">Something</h1>
+      <div className="text-center mb-10">
+        <Button
+          onClick={() => setView("all")}
+          className="px-4 py-2 mx-2 rounded text-white bg-primary-500 hover:bg-primary-600 transition-all duration-700"
+        >
+          Show All Projects
+        </Button>
+        <Button
+          onClick={() => setView("frontend")}
+          className="px-4 py-2 mx-2 rounded text-primary-800 bg-accent-400 hover:bg-accent-500 transition-all duration-700"
+        >
+          Show Frontend Projects
+        </Button>
+        <Button
+          onClick={() => setView("fullstack")}
+          className="px-4 py-2 mx-2 rounded text-white bg-background-500 hover:bg-accent-700 transition-all duration-700"
+        >
+          Show Full Stack Projects
+        </Button>
       </div>
-      <FullStackProjects />
-      <FrontendProjects />
+
+      <div>
+        {view === "all" && (
+          <>
+            <FullStackProjects />
+            <FrontendProjects />
+          </>
+        )}
+        {view === "frontend" && <FrontendProjects />}
+        {view === "fullstack" && <FullStackProjects />}
+      </div>
     </>
   );
 }
